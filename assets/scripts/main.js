1b40484cccc4c0eb67c4381fb37bea56f84d66c6
Hoang Tam <goddie9x@yahoo.com>
2021-06-02 03:00:37 +0700

    update current page active\

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,21 +1,21 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 const $AO = function activeOn(elements) {
     elements.forEach(element => {
         element.classList.add('active');
     })
 }
-
 const $AOff = function activeOff(elements) {
     elements.forEach(element => {
         element.classList.remove('active');
     })
 }
-
 const $toggle = function toggleActiveClass(elements) {
     elements.forEach(element => {
         element.classList.toggle('active');
     })
 }
+
 $('.main-nav-moblie').onclick = function() {
-    console.log([...$$('.menu-icon'), $('.main-nav-bar')])
     $toggle([...$$('.menu-icon'), $('.main-nav-bar')]);
 }
